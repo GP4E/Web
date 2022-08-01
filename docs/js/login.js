95b69788a9f90a4e484a1a16b1c3e15d441fc002
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-08-01 19:28:31 +0200

    upd

---
+++ docs/js/login.js
@@ -0,0 +1,5 @@
+//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
+//473052494821-94r369taon7n36c5justn4kplr641qg0.apps.googleusercontent.com
+//https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid
+//https://console.cloud.google.com/apis/credentials?highlightClient=473052494821-94r369taon7n36c5justn4kplr641qg0.apps.googleusercontent.com&project=gp4e-6a225
+//https://gp4e.github.io/Web/home.html
