db871c151f0181d8fb9ca57ec508792ad92109ef
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-08-02 15:34:26 +0200

    css

--- docs/js/query.js
+++ docs/js/query.js
@@ -1,9 +1,11 @@
 function query() {
-    var pairs = location.search.slice(1).split('&');
     var result = {};
-    pairs.forEach(function(pair) {
-        pair = pair.split('=');
-        result[pair[0]] = decodeURIComponent(pair[1] || '');
-    });
+    if (location.search.length>0) {
+        var pairs = location.search.slice(1).split('&');
+        pairs.forEach(function(pair) {
+            pair = pair.split('=');
+            result[pair[0]] = decodeURIComponent(pair[1] || '');
+        });
+    }
     return result
 }
