e067697e84d26535536a3f4ba8a6f8c815fb9e1f
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-08-04 17:50:19 +0200

    up

--- docs/js/gitpath.js
+++ docs/js/gitpath.js
@@ -45,7 +45,7 @@ function analyze(t) {
                 img.innerHTML='<path d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3H7.5a.25.25 0 01-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75z"></path>'
                 break;
             default:
-                img.style.display="hidden"
+                // img.style.display="hidden"
         }
         var label=document.createElement("div")
         label.classList.add("github_item_label")
@@ -53,8 +53,15 @@ function analyze(t) {
         label.setAttribute("sha",p.sha)
         label.setAttribute("mode",p.mode)
         label.setAttribute("url",p.url)
+        var commitchanges = document.createElement("div")
+        commitchanges.classList.add("github_item_commitchanges")
+        var size = document.createElement("div")
+        size.classList.add("github_item_size")
+        if (!nou(p.size)) size.innerHTML=p.size
         item.appendChild(img)
         item.appendChild(label)
+        item.appendChild(commitchanges)
+        item.appendChild(size)
         item.addEventListener("click", (c)=>{document.dispatchEvent(new Event("github_navigate_to",{item: p}))})
         return item
     })
