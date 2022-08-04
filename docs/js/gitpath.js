fc843bf6413a2d4f4b34d9e306de03c0a0fc1b7d
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-08-04 15:59:51 +0200

    upd

--- docs/js/gitpath.js
+++ docs/js/gitpath.js
@@ -1,13 +1,16 @@
-function setupGitGraphicsPage(element) {
+async function setupGitGraphicsPage(element) {
+    element.innerHTML = '<span class="awaitloading">Stiamo caricando la pagina.</span>'
     var res = ""
-    console.log()
-    
-    element.innerHTML=res
+    var tree = await req("repos/GP4E/GP4EGame/git/trees/ea035f6f977e1d3c86623e7c2522592c277e2872",(x)=>{console.log(x)})
+    var trhtml = document.createElement("div")
+    analyze(tree).forEach(e=>trhtml.appendChild(e))
+    trhtml.classList.add("github_path_tree")
+    element.appendChild(trhtml)
 }
 
-function req(gurl, callback_success) {
+async function req(gurl, callback_success) {
     var t = window.localStorage.getItem("github_token")
-    $.ajax({
+    return (await $.ajax({
         url: 'https://api.github.com/'+gurl,
         beforeSend: function(xhr) {
             xhr.setRequestHeader("Accept", "application/vnd.github+json")
@@ -19,5 +22,37 @@ function req(gurl, callback_success) {
         success: function (data) {
             callback_success(data)
         }
+    }))
+}
+
+function analyze(t) {
+    var treepath = Array.from(t.tree)
+    var res = treepath.map(p=>{
+        var item = document.createElement("div")
+        item.classList.add("item")
+        item.classList.add("github_item_type_"+p.type)
+        var img=document.createElement("div")
+        img.classList.add("github_item_icon")
+        switch (p.type) {
+            case "blob":
+                img.setAttribute("src","./icons/file.svg")
+                break;
+            case "tree":
+                img.setAttribute("src","./icons/tree.svg")
+                break;
+            default:
+                img.style.display="hidden"
+        }
+        var label=document.createElement("div")
+        label.classList.add("github_item_label")
+        label.innerText=p.path
+        label.setAttribute("sha",p.sha)
+        label.setAttribute("mode",p.mode)
+        label.setAttribute("url",p.url)
+        item.appendChild(img)
+        item.appendChild(label)
+        item.addEventListener("click", (c)=>{document.dispatchEvent(new Event("github_navigate_to",{item: p}))})
+        return item
     })
+    return res
 }
