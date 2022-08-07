ec9604082c44026f829491a715893fa3ad096a20
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-08-08 00:50:51 +0200

    save

--- docs/js/mydiscord.js
+++ docs/js/mydiscord.js
@@ -1,7 +1,7 @@
 async function dsreq(dsurl,callback_success) {
     var t = window.localStorage.getItem("discord_token")
     return (await $.ajax({
-        url: 'https://api.github.com/'+gurl,
+        url: 'https://discord.com/api/webhooks/1005863439685668914/EV7WlCmhzWC14XJGKdo55GG8xcp_kx73lIeiU4B6GmLsCI3s9CN-glzyj60dTKSplz-Q/'+gurl,
         beforeSend: function(xhr) {
             xhr.setRequestHeader("Content-Type", "application/json")
             xhr.setRequestHeader("Authorization", "token "+t)
